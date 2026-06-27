49baa797859f2fea91705ada85f9164125084056
杨福绅 <fushenyang@gmail.com>
2026-06-27 21:36:53 +0800

    再修改一点点

--- src/infrastructure/LocalFileSink.ts
+++ src/infrastructure/LocalFileSink.ts
@@ -4,15 +4,12 @@ import { Effect, Sink, Chunk } from "effect";
 
 // 明确表明这是本地文件系统的实现
 export const makeLocalCsvSink = (filePath: string) =>
-  Sink.unwrapScoped(
-    Effect.gen(function* () {
-      const fs = yield* FileSystem.FileSystem;
-      const file = yield* fs.open(filePath, { flag: "w" });
-      const encoder = new TextEncoder();
-
-      return Sink.forEachChunk((chunk: Chunk.Chunk<string>) => {
-        const lines = [...chunk].join("\n") + "\n";
-        return file.write(encoder.encode(lines));
-      });
-    }),
-  );
+  Effect.gen(function* () {
+    const fs = yield* FileSystem.FileSystem;
+    const file = yield* fs.open(filePath, { flag: "w" });
+    const encoder = new TextEncoder();
+    return Sink.forEachChunk((chunk: Chunk.Chunk<string>) => {
+      const lines = [...chunk].join("\n") + "\n";
+      return file.write(encoder.encode(lines));
+    });
+  }).pipe(Sink.unwrapScoped);
